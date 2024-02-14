ccd167956bb17350bcb1c1d08df35a8d308c65a7
28606959 <alix.paradis@etu.sorbonne-universite.fr>
2024-02-14 14:20:32 +0900

    2024/02/14

--- beginner/340/D.py
+++ beginner/340/D.py
@@ -1,6 +1,7 @@
 import sys, math
 from collections import deque
 from heapq import heappop, heappush
+from numpy import Inf
 def IIS(): return map(int, input().split())
 def LIIS(): return list(IIS())
 def SIIS(): return set(IIS())
@@ -13,28 +14,29 @@ sys.setrecursionlimit(10**8)
 
 n = II()
 abx = list()
-for _ in range(n-1):
-  tmp_ls = LIIS()
-  abx.append(tmp_ls)
-
-ls_cost = [0]*(n+1)
-
+graph = dict()
+for i in range(n):
+  graph[i] = list()
 for i in range(n-1):
-  ls_cost[i+2] = ls_cost[i+1] + abx[i][0]
+  a, b, x = IIS()
+  graph[i].append((i+1, a))
+  graph[i].append((x-1, b))
 
-h = [[0, 1]]
-nb_e = 1
-while nb_e > 0:
-  e = heappop(h)
-  nb_e -= 1
-  if e[0] > ls_cost[e[1]]:
-    continue
-  ls_cost[e[1]] = e[0]
-  if e[1] < n-1:
-    heappush(h, [e[0]+abx[e[1]-1][0], e[1]+1])
-    nb_e += 1
-  if e[1] != n:
-    heappush(h, [e[0]+abx[e[1]-1][1], abx[e[1]-1][2]])
-    nb_e += 1
+def lazyDijkstras(graph, root):
+  n = len(graph)
+  dist = [Inf for _ in range(n)]
+  dist[root] = 0
+  visited = [False for _ in range(n)]
+  pq = [(0, root)]
+  while len(pq) > 0:
+    _, u = heappop(pq)
+    if visited[u]:
+      continue
+    visited[u] = True
+    for v, l in graph[u]:
+      if dist[u] + l < dist[v]:
+        dist[v] = dist[u] + l
+        heappush(pq, (dist[v], v))
+  return dist
 
-print(ls_cost[n])
+print(lazyDijkstras(graph,0)[n-1])
